a38a91946efc1daa677300c657b5883b3dbe9337
rismamardhiati-creator <rismamardhiati@gmail.com>
2026-07-31 08:22:57 +0700

    Add files via upload

---
+++ string.php
@@ -0,0 +1,9 @@
+<?php
+$namaDepan = "Budi";
+$namaBelakang = "Santoso";
+$pesan = "Selamat belajar PHP dasar!";
+
+echo "--- Tipe Data String ---<br>";
+echo "Nama Lengkap: " . $namaDepan . " " . $namaBelakang . "<br>";
+echo "Pesan: " . $pesan;
+?>
