854dcd14040ed28d71bb3bac04b4fb3257670755
rismamardhiati-creator <rismamardhiati@gmail.com>
2026-08-05 10:43:40 +0700

    Delete string.php

--- string.php
+++
@@ -1,9 +0,0 @@
-<?php
-$namaDepan = "Risma";
-$namaBelakang = "Mardhiati";
-$pesan = "Selamat belajar PHP dasar!";
-
-echo "--- Tipe Data String ---<br>";
-echo "Nama Lengkap: " . $namaDepan . " " . $namaBelakang . "<br>";
-echo "Pesan: " . $pesan;
-?>
