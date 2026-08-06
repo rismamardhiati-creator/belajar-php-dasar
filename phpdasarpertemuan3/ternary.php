02574abd9c4ab184bd4a257652cabe8d71437954
rismamardhiati-creator <rismamardhiati@gmail.com>
2026-08-06 08:40:29 +0700

    Add files via upload

---
+++ phpdasarpertemuan3/ternary.php
@@ -0,0 +1,7 @@
+<?php
+$nilai = 22;
+
+// Menggunakan operator ternary untuk mengecek apakah nilai positif atau negatif
+$hasil = ($nilai >= 30) ? "Nilai positif" : "Nilai negatif";
+
+echo "Hasil: $hasil <br>";
