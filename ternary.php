1441a15251a887dafa869f6715c6e84436badd47
rismamardhiati-creator <rismamardhiati@gmail.com>
2026-08-06 08:40:00 +0700

    Delete ternary.php

--- ternary.php
+++
@@ -1,7 +0,0 @@
-<?php
-$nilai = 22;
-
-// Menggunakan operator ternary untuk mengecek apakah nilai positif atau negatif
-$hasil = ($nilai >= 30) ? "Nilai positif" : "Nilai negatif";
-
-echo "Hasil: $hasil <br>";
